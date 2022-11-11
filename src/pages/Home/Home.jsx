2020aee8871418a4ba67fd8b424c25862aa9dc07
Shreyas Kasliwal <kasliwalshreyas07@gmail.com>
2022-11-12 03:20:33 +0530

    added demo of recat sketch canvas to home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,44 @@
-import { Fragment } from "react";
+import { Flex, FormControl, FormLabel, Input } from "@chakra-ui/react";
+import { Fragment, useRef } from "react";
+import { ReactSketchCanvas } from 'react-sketch-canvas';
+
+
 
 const Home = () => {
+
+  // const [canvas, setCanvas] = useState(null);
+  const canvas = useRef(null);
+
+  const styles = {
+    border: '0.0625rem solid #9c9c9c',
+    borderRadius: '0.25rem',
+  };
+
+  const exportImage = async () => {
+    const dataURL = await canvas.current.exportImage('png')
+      .then(data => {
+        console.log(data);
+      })
+      .catch(err => console.log(err.message))
+  };
+
+
   return (
     <Fragment>
       <h1>Home</h1>
       <p>Home Page</p>
+
+      <ReactSketchCanvas
+        ref={canvas}
+        style={styles}
+        width="50%"
+        height="50%"
+        strokeWidth={4}
+        strokeColor="black"
+      />
+      <button onClick={exportImage}>Get Image</button>
+
+
     </Fragment>
   );
 };
